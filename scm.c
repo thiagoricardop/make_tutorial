e30b365e1b4a14b96f5e8e00e3f91c4506c67ff8
thiagoricardop <t.ricardooliveira08@gmail.com>
2025-02-24 10:22:50 -0500

    adicionando arquivo bin.

--- scm.c
+++ scm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
@@ -9,7 +10,10 @@ void multiplica(int a, int b) {
 }
 
 int main() {
-    printf("Olá, Estudante!\n");
+    time_t local; //"local" is a variable of "time_t" data type.
+    time(&local);
+    
+    printf("Olá, Estudante!\nA data e hora locais são: %s", ctime(&local));
     soma(5, 3);
     multiplica(5, 3);
     return 0;
